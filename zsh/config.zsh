98ebab833da9107d94f46eacc70c4a621727f770
Miguel Perez <miguel250@gmail.com>
2011-09-08 02:27:19 -0400

    add bind key

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -44,3 +44,11 @@ zstyle ':completion:*' insert-tab pending
 fpath=($ZSH/completions $fpath)
 
 autoload -U $ZSH/completions/*(:t)
+
+bindkey '^[^[[D' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[[5D' beginning-of-line
+bindkey '^[[5C' end-of-line
+bindkey '^[[3~' delete-char
+bindkey '^[^N' newtab
+bindkey '^?' backward-delete-char
