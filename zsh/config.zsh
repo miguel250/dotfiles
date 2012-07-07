1ef57a8c44cf0d5e79431002fc4cb91cebb18cfa
Miguel Perez <miguel250@gmail.com>
2012-07-07 14:53:06 -0400

    Add basic zsh setup

---
+++ zsh/config.zsh
@@ -0,0 +1,21 @@
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_verify
+setopt inc_append_history
+setopt share_history 
+
+setopt complete_aliases
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS
+setopt LOCAL_TRAPS 
+setopt PROMPT_SUBST
+setopt CORRECT
+setopt COMPLETE_IN_WORD
+setopt IGNORE_EOF
---
+++ zsh/theme.zsh
@@ -0,0 +1,37 @@
+autoload colors && colors
+bell=`tput bel` 
+# cheers, @ehrenmurdick
+# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+
+git_dirty() {
+  st=$(git status 2>/dev/null | tail -n 1)
+  if [[ $st == "" ]]
+  then
+    echo ""
+  else
+    if [[ $st == "nothing to commit (working directory clean)" ]]
+    then
+      echo "[%{$fg_bold[cyan]%}$(git_prompt_info)%{$reset_color%}]"
+    else
+      echo "[%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}]"
+    fi
+  fi
+}
+
+git_prompt_info () {
+ ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+ echo "${ref#refs/heads/}"
+}
+
+directory_name(){
+  echo "%{%b%F{green}%K{black}%}%~%{%B%F{green}%}"
+}
+
+export PROMPT=$'$(directory_name) $(git_dirty)› '
+
+
+precmd() {
+  echo -n "\033]1;$bell\033]2;$PWD > - $USERNAME@$HOST $bell" 
+
+}
