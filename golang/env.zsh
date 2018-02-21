c9f2fb1dfb39c9fd4b609babb61f9d9f2a65930b
Miguel <miguel@miguelpz.com>
2018-02-21 13:39:20 -0500

    Add message for when we are downloading golang

--- golang/env.zsh
+++ golang/env.zsh
@@ -1,4 +1,19 @@
 if which gvm  > /dev/null 2>&1
 then
+  plataform="$(uname -m)"
+  name="$(echo "$(uname)" | awk '{print tolower($0)}')"
+  arch="UNKNOWN"
+
+  if [ $plataform = "x86_64" ]
+  then
+    arch="amd64"
+  fi
+
+  go_directory="$HOME/.gvm/versions/go$GO_VERSION.$name.$arch"
+
+  if [[ ! -d "$go_directory" ]]
+  then
+    echo "We might be installing go. This might take a while."
+  fi
   eval "$(gvm $GO_VERSION)"
 fi
