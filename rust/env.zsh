acf3b55044f756351e779b361a0a4940789a2634
Miguel <miguel@miguelpz.com>
2018-04-07 18:31:34 +0000

    Only set rust default if it is a different version

--- rust/env.zsh
+++ rust/env.zsh
@@ -1,4 +1,15 @@
+set ex
 if which rustup > /dev/null 2>&1
 then
-  rustup default $RUST_VERSION &> /dev/null
+  plataform="$(uname -m)"
+  name="$(echo "$(uname)" | awk '{print tolower($0)}')"
+  arch="unknown"
+
+  name="$RUST_VERSION-$plataform-$arch-$name-gnu"
+
+  if ! grep -q "$name" $HOME/.rustup/settings.toml
+  then
+    echo "Installing rust $RUST_VERSION"
+    rustup default $RUST_VERSION
+  fi
 fi
