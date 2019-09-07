ec9be4d34cf4f7ca0031b3a2dffa817540873ca1
Miguel Perez <miguel@miguelpz.com>
2019-09-07 13:41:37 -0400

    Update plugs to latest version

--- zplug/env.zsh
+++ zplug/env.zsh
@@ -1,18 +1,18 @@
 source ~/.zplug/init.zsh
 
 zplug 'mfaerevaag/wd', \
-  at:3cbc583, \
+  at:303e68f, \
   as:command, use:"wd.sh", \
   hook-load:"wd() { . $ZPLUG_REPOS/mfaerevaag/wd/wd.sh }"
 
 zplug "tarruda/zsh-autosuggestions", \
-  at:e9d8231ba7dfbf7afb3f2a57ae00b803b4ae9f83
+  at:43f3bc4010b2c697d2252fdd8b36a577ea125881
 
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 
 zplug "plugins/extract", \
-  at:ab9d92f, \
+  at:586ca16, \
   from:oh-my-zsh
 
 if ! zplug check; then
