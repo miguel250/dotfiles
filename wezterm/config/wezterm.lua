0a1a18473aa05a229bb835a112c981f8fdf24f8d
Miguel <miguel@miguelpz.com>
2024-09-19 19:26:03 -0400

    Fix issue Wezterm not working correctly on wayland
    - Formatting changes

--- wezterm/config/wezterm.lua
+++ wezterm/config/wezterm.lua
@@ -1,68 +1,67 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 config.check_for_updates = false
 config.enable_tab_bar = false -- we are still using tmux
 
 config.font_size = 16
-config.font =
-    wezterm.font {
-        family = "JetBrains Mono"
-    }
+config.font = wezterm.font({
+	family = "JetBrains Mono",
+})
 
 config.disable_default_key_bindings = true
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW"
 config.window_background_opacity = 0.7
+
+if wezterm.target_triple:find("linux") ~= nil then
+	-- See: https://github.com/wez/wezterm/issues/5931
+	config.enable_wayland = false
+end
 
 local default_mods = "CTRL"
 if wezterm.target_triple:find("darwin") ~= nil then
-    default_mods = "CMD"
+	default_mods = "CMD"
+
+	wezterm.on("window-resized", function(window, _)
+		local dimensions = window:get_dimensions()
+		local top = "1.2cell"
+
+		if dimensions.is_full_screen then
+			top = "0"
+		end
+		local overrides = window:get_config_overrides() or {}
+
+		overrides.window_padding = {
+			top = top,
+		}
+		window:set_config_overrides(overrides)
+	end)
+
+	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+	config.window_padding = {
+		top = "1.2cell",
+	}
 end
-
-wezterm.on(
-    'window-resized',
-    function(window, _)
-        local dimensions = window:get_dimensions()
-        local top = '1.2cell'
-
-        if dimensions.is_full_screen then
-            top = '0'
-        end
-        local overrides = window:get_config_overrides() or {}
-
-        overrides.window_padding = {
-            top = top,
-        }
-        window:set_config_overrides(overrides)
-    end
-)
-
-
-config.window_padding = {
-    top = '1.2cell',
-}
-
 
 config.color_scheme = "Catppuccin Mocha"
 
 local act = wezterm.action
 config.keys = {
-    { key = ")",     mods = default_mods, action = act.ResetFontSize },
-    { key = "-",     mods = default_mods, action = act.DecreaseFontSize },
-    { key = "=",     mods = default_mods, action = act.IncreaseFontSize },
-    { key = "v",     mods = default_mods, action = act.PasteFrom("Clipboard") },
-    { key = "Copy",  mods = "NONE",       action = act.CopyTo("Clipboard") },
-    { key = "Paste", mods = "NONE",       action = act.PasteFrom("Clipboard") },
-    { key = "F11",   mods = default_mods, action = act.ToggleFullScreen }
+	{ key = ")", mods = default_mods, action = act.ResetFontSize },
+	{ key = "-", mods = default_mods, action = act.DecreaseFontSize },
+	{ key = "=", mods = default_mods, action = act.IncreaseFontSize },
+	{ key = "v", mods = default_mods, action = act.PasteFrom("Clipboard") },
+	{ key = "Copy", mods = "NONE", action = act.CopyTo("Clipboard") },
+	{ key = "Paste", mods = "NONE", action = act.PasteFrom("Clipboard") },
+	{ key = "F11", mods = default_mods, action = act.ToggleFullScreen },
 }
 
 config.mouse_bindings = {
-    -- Ctrl-click will open the link under the mouse cursor
-    {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "CTRL",
-        action = wezterm.action.OpenLinkAtMouseCursor
-    }
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
 }
 
 return config
